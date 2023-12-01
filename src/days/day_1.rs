a2400f468aeb1add54c361560de15e6f1d9d8881
Adam Dormier <adam.dormier@gmail.com>
2023-12-01 12:37:36 -0800

    solve 1.2

--- src/days/day_1.rs
+++ src/days/day_1.rs
@@ -8,6 +8,10 @@ pub fn run(mut input_file: File) -> AdventResult<()> {
     utils::part_header(1);
     part_1(&calibration_doc)?;
 
+    // Part 2
+    utils::part_header(2);
+    part_2(&calibration_doc)?;
+
     Ok(())
 }
 
@@ -18,26 +22,94 @@ fn part_1(calibration_lines: &[String]) -> AdventResult<()> {
         .sum::<AdventResult<u32>>()?;
 
     println!("Sum of calibration values: {calibration_sum}");
-    Ok(())
+    return Ok(());
+
+    fn calibration_value(line: &str) -> AdventResult<u32> {
+        let first_digit = line
+            .chars()
+            .filter_map(|c| char::to_digit(c, 10))
+            .next()
+            .ok_or_else(|| {
+                AdventErr::InputParse(format!("Failed to find first digit in line:\n{line}"))
+            })?;
+
+        let last_digit = line
+            .chars()
+            .rev()
+            .filter_map(|c| char::to_digit(c, 10))
+            .next()
+            .ok_or_else(|| {
+                AdventErr::InputParse(format!("Failed to find last digit in line:\n{line}"))
+            })?;
+
+        Ok(10 * first_digit + last_digit)
+    }
 }
 
-fn calibration_value(line: &str) -> AdventResult<u32> {
-    let first_digit = line
-        .chars()
-        .filter_map(|c| char::to_digit(c, 10))
-        .next()
-        .ok_or_else(|| {
-            AdventErr::InputParse(format!("Failed to find first digit in line:\n{line}"))
-        })?;
+fn part_2(calibration_lines: &[String]) -> AdventResult<()> {
+    fn calibration_value(line: &str) -> u32 {
+        const DIGIT_STRINGS: [&str; 9] = [
+            "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+        ];
 
-    let last_digit = line
-        .chars()
-        .rev()
-        .filter_map(|c| char::to_digit(c, 10))
-        .next()
-        .ok_or_else(|| {
-            AdventErr::InputParse(format!("Failed to find last digit in line:\n{line}"))
-        })?;
+        // First digit
 
-    Ok(10 * first_digit + last_digit)
+        // Find actual digit
+        let (mut earliest_index, mut first_digit) = line
+            .chars()
+            .enumerate()
+            .filter_map(|(index, c)| char::to_digit(c, 10).map(|value| (index, value)))
+            .next()
+            .unwrap_or((usize::MAX, 0));
+
+        // Check words
+        for (value, digit_name) in DIGIT_STRINGS
+            .iter()
+            .enumerate()
+            .map(|(index, name)| (index + 1, name))
+        {
+            if let Some(index) = line.find(digit_name) {
+                if index < earliest_index {
+                    earliest_index = index;
+                    first_digit = value as u32;
+                }
+            }
+        }
+
+        // Last digit
+        let char_count = line.chars().count();
+
+        // Find actual digit
+        let (mut latest_index, mut last_digit) = line
+            .chars()
+            .rev()
+            .enumerate()
+            .filter_map(|(index, c)| char::to_digit(c, 10).map(|value| (char_count - index - 1, value)))
+            .next()
+            .unwrap_or((0usize, 0));
+
+        // Check words
+        for (value, digit_name) in DIGIT_STRINGS
+            .iter()
+            .enumerate()
+            .map(|(index, name)| (index + 1, name))
+        {
+            if let Some(index) = line.rfind(digit_name) {
+                if index > latest_index {
+                    latest_index = index;
+                    last_digit = value as u32;
+                }
+            }
+        }
+
+        10 * first_digit + last_digit
+    }
+
+    let calibration_sum: u32 = calibration_lines
+        .iter()
+        .map(|line| calibration_value(line))
+        .sum();
+
+    println!("Sum of calibration values: {calibration_sum}");
+    Ok(())
 }
