a21ee6dfd4c62f0a083b5c70dfad548429e16af9
Adam Dormier <adam.dormier@gmail.com>
2023-12-05 22:33:15 -0800

    solve 6.2

--- src/days/day_6.rs
+++ src/days/day_6.rs
@@ -4,34 +4,47 @@ use std::fs::File;
 use std::io::Read;
 
 pub fn run(mut input_file: File) -> AdventResult<()> {
-    let races = parse_input(&mut input_file)?;
+    let mut input = String::new();
+    input_file.read_to_string(&mut input)?;
 
     // Part 1
+    let races = parse_multiple_races(&input)?;
     utils::part_header(1);
     part_1(&races);
+
+    // Part 2
+    let race = parse_single_race(&input)?;
+    utils::part_header(2);
+    part_2(race);
 
     Ok(())
 }
 
 fn part_1(races: &[Race]) {
-    let record_beating_product: u32 = races.iter().map(|race| race.winning_options()).product();
+    let record_beating_product: u64 = races.iter().map(|race| race.winning_options()).product();
 
-    println!("Product of ways to win: {record_beating_product}");
+    println!("Product of ways to beat records: {record_beating_product}");
 }
 
-fn compute_distance(hold_time: u32, total_race_time: u32) -> u32 {
+fn part_2(race: Race) {
+    let ways_to_beat_record = race.winning_options();
+
+    println!("Ways to beat record: {ways_to_beat_record}");
+}
+
+fn compute_distance(hold_time: u64, total_race_time: u64) -> u64 {
     debug_assert!(hold_time <= total_race_time);
     hold_time * (total_race_time - hold_time)
 }
 
 #[derive(Debug, Copy, Clone)]
 struct Race {
-    time: u32,
-    record_distance: u32,
+    time: u64,
+    record_distance: u64,
 }
 
 impl Race {
-    fn min_win_hold_time(&self) -> Option<u32> {
+    fn min_win_hold_time(&self) -> Option<u64> {
         let time = self.time as f64;
         let distance = self.record_distance as f64;
 
@@ -40,7 +53,7 @@ impl Race {
             return None;
         }
 
-        let mut min_hold_time = min_hold_time as u32;
+        let mut min_hold_time = min_hold_time as u64;
 
         // In the case where the root is actually an integer, we want to exclude the root itself.
         // Check just this one case.
@@ -54,7 +67,7 @@ impl Race {
         Some(min_hold_time)
     }
 
-    fn winning_options(&self) -> u32 {
+    fn winning_options(&self) -> u64 {
         match self.min_win_hold_time() {
             None => 0,
             Some(min_time) => self.time - 2 * min_time + 1,
@@ -62,15 +75,8 @@ impl Race {
     }
 }
 
-fn parse_input(input_file: &mut File) -> AdventResult<Vec<Race>> {
-    let mut input = String::new();
-    input_file.read_to_string(&mut input)?;
-
-    let mut iter = input.lines();
-
-    let (Some(time_line), Some(distance_line)) = (iter.next(), iter.next()) else {
-        return Err(InputParse(String::from("Failed to find 2 lines in input")));
-    };
+fn parse_multiple_races(input: &str) -> AdventResult<Vec<Race>> {
+    let (time_line, distance_line) = extract_lines(input)?;
 
     let races = time_line
         .split_whitespace()
@@ -89,4 +95,39 @@ fn parse_input(input_file: &mut File) -> AdventResult<Vec<Race>> {
         .collect::<AdventResult<_>>()?;
 
     Ok(races)
+}
+
+fn parse_single_race(input: &str) -> AdventResult<Race> {
+    let (time_line, distance_line) = extract_lines(input)?;
+
+    let Some((_, time)) = time_line.split_once(':') else {
+        return Err(InputParse(String::from("Failed to remove Time header")));
+    };
+    let time = broken_str_to_u64(time);
+
+    let Some((_, distance)) = distance_line.split_once(':') else {
+        return Err(InputParse(String::from("Failed to remove Distance header")));
+    };
+    let record_distance = broken_str_to_u64(distance);
+
+    Ok(Race {
+        time,
+        record_distance,
+    })
+}
+
+fn extract_lines(input: &str) -> AdventResult<(&str, &str)> {
+    let mut iter = input.lines();
+
+    let (Some(time_line), Some(distance_line)) = (iter.next(), iter.next()) else {
+        return Err(InputParse(String::from("Failed to find 2 lines in input")));
+    };
+
+    Ok((time_line, distance_line))
+}
+
+fn broken_str_to_u64(s: &str) -> u64 {
+    s.chars()
+        .filter(|c| c.is_digit(10))
+        .fold(0, |acc, x| acc * 10 + x.to_digit(10).unwrap() as u64)
 }
